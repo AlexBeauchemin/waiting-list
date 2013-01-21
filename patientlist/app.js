f2739aee0e92a4d3194c4ef3dd3bff5ad6ef22c6
Alex Beauchemin <alexbeauchemin01@gmail.com>
2013-01-21 17:23:55 -0500

    Jqueryui + sortable

--- patientlist/app.js
+++ patientlist/app.js
@@ -1,49 +1,77 @@
-// Set up a collection to contain patient information. On the server,
-// it is backed by a MongoDB collection named "patients".
 Patients = new Meteor.Collection("patients");
 
 if (Meteor.isClient) {
-    Meteor.subscribe('patients');
-    console.log(Patients.find().count());
-    Template.patientlist.patients = function () {
-        return Patients.find({}, {sort: {score: -1, name: 1}});
-    };
+		Meteor.subscribe('patients');
+		Template.patientlist.patients = function () {
+				return Patients.find({}, {sort: {position: 1, name: -1}});
+		};
 
-    Template.patientlist.selected_name = function () {
-        var patient = Patients.findOne(Session.get("selected_patient"));
-        return patient && patient.name;
-    };
+		Template.patientlist.selected_name = function () {
+				var patient = Patients.findOne(Session.get("selected_patient"));
+				return patient && patient.name;
+		};
 
-    Template.patient.selected = function () {
-        return Session.equals("selected_patient", this._id) ? "selected" : '';
-    };
+		Template.patient.selected = function () {
+				return Session.equals("selected_patient", this._id) ? "selected" : '';
+		};
 
-    Template.patientlist.events({
-        'click input.inc': function () {
-            Patients.update(Session.get("selected_patient"), {$inc: {score: 5}});
-        }
-    });
+		Template.patientlist.events({
+				'click input.up': function () {
+						if(Session.get("selected_patient_position")>1){
+							var switching_patient = Patients.findOne({position: Session.get("selected_patient_position")-1});
+							Patients.update(Session.get("selected_patient"), {$inc: {position: -1}});
+							Patients.update(switching_patient._id,{$inc: {position: +1}});
+							Session.set("selected_patient_position", Session.get("selected_patient_position")-1);
+						}
+				},
+				'click input.down': function () {
+						var switching_patient = Patients.findOne({position: Session.get("selected_patient_position")+1});
+						if(switching_patient){
+							Patients.update(Session.get("selected_patient"), {$inc: {position: 1}});
+							Patients.update(switching_patient._id,{$inc: {position: -1}});
+							Session.set("selected_patient_position", Session.get("selected_patient_position")+1);
+						}
+				},
+				'click input.reset': function () {
+						var patients = Patients.find(),
+								count = 0;
 
-    Template.patient.events({
-        'click': function () {
-            Session.set("selected_patient", this._id);
-        }
-    });
+						patients.forEach(function (patient) {
+							Patients.update(patient._id, {$set: {position: count+1}});
+							count += 1;
+						});
+				}
+		});
+
+		Template.patientlist.rendered = function() {
+				$(".patientlist").sortable();
+		};
+
+		Template.patient.events({
+				'click': function () {
+						Session.set("selected_patient", this._id);
+						Session.set("selected_patient_position", this.position);
+				}
+		});
+
 }
 
-// On server startup, create some patients if the database is empty.
+
+
+
+
 if (Meteor.isServer) {
     Meteor.publish("patients", function(test) {
         return Patients.find({});
     });
     Meteor.startup(function () {
         if (Patients.find().count() === 0) {
-            var names = ["Ada Lovelace",
-                "Grace Hopper",
-                "Marie Curie",
-                "Carl Friedrich Gauss",
-                "Nikola Tesla",
-                "Claude Shannon"];
+						var names = ["Ada Lovelace",
+					                "Grace Hopper",
+					                "Marie Curie",
+					                "Carl Friedrich Gauss",
+					                "Nikola Tesla",
+					                "Claude Shannon"];
             for (var i = 0; i < names.length; i++)
                 Patients.insert({name: names[i], position: i});
         }
