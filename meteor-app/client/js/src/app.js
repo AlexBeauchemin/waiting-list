ffd3af6425970ff9c643e1130c013f43b3dfedd4
Alex Beauchemin <alexbeauchemin01@gmail.com>
2014-02-07 16:48:59 -0500

    Restructuration

--- meteor-app/client/js/src/app.js
+++ meteor-app/client/js/src/app.js
@@ -1,247 +1,253 @@
-(function ($) {
-  App = {
-    $body: null,
-    $alerts: null,
-    currentPage: 'main',
-    spinner: null,
-    spinner_opts: {
-      lines: 11, // The number of lines to draw
-      length: 0, // The length of each line
-      width: 5, // The line thickness
-      radius: 11, // The radius of the inner circle
-      corners: 1, // Corner roundness (0..1)
-      rotate: 0, // The rotation offset
-      color: '#000', // #rgb or #rrggbb
-      speed: 1.3, // Rounds per second
-      trail: 54, // Afterglow percentage
-      shadow: true, // Whether to render a shadow
-      hwaccel: true, // Whether to use hardware acceleration
-      className: 'spinner', // The CSS class to assign to the spinner
-      zIndex: 2e9, // The z-index (defaults to 2000000000)
-      top: '20px', // Top position relative to parent in px
-      left: '20px' // Left position relative to parent in px
-    },
-    viewmode_properties: null,
+console.log('main');
 
-    init: function () {
-      var _this = this;
-      _this.$body = $('body');
-      _this.$alerts = _this.$body.find('.alerts ul');
-    },
+Template.login.stateIs = function (state) {
+  return state === Session.get("loginState");
+};
 
-    bindEvents: function () {
-      var _this = this;
+Template.institutionslist.institutions = function () {
+  var user = Meteor.user();
+  //TODO: Filter to not see private lists must be done server side
 
-      _this.$body.on('click', '.no-link', function () {
-        event.stopPropagation();
-        return false;
+  if (user) {
+    return Institutions.find({"users": {$ne: user._id}, "private": "0"}, {sort: {name: 1}});
+  }
+
+  return Institutions.find({"private": "0"}, {sort: {name: 1}});
+};
+
+Template.institutionslist.myInstitutions = function () {
+  var user = Meteor.user();
+  if (user) {
+    return Institutions.find({users: user._id}, {sort: {name: 1}});
+  }
+  return "";
+};
+
+Template.institutionslist.user = function () {
+  return Meteor.user();
+};
+
+Template.patientlist.patients = function () {
+  var patients = Patients.find({}, {sort: {position: 1, name: -1}});
+  return patients.fetch();
+};
+
+Template.patientlist.selected_name = function () {
+  var patient = Patients.findOne(Session.get("selected_patient"));
+  return patient && patient.name;
+};
+
+Template.patientlist.institution_info = function () {
+  var institution = Institutions.findOne({_id: Session.get('current_institution')});
+  if (institution)
+    return institution;
+  return {};
+};
+
+Template.patientlist.isAdmin = function () {
+  var institution = Institutions.findOne({_id: Session.get('current_institution')});
+  if (!institution || !institution.users)
+    return false;
+  return ($.inArray(Meteor.user()._id, institution.users) != -1);
+};
+
+Template.patient.active = function () {
+  return Session.equals("selected_patient", this._id) ? "active" : '';
+};
+
+Template.institution.active = function () {
+  return Session.equals("current_institution", this._id) ? "active" : '';
+};
+
+
+//EVENTS
+Template.nav.events({
+  'click .login': function () {
+    App.changePage('login');
+  },
+  'click .profil': function () {
+    App.changePage('profil');
+  },
+  'click .logout': function () {
+    Meteor.logout(function (error) {
+      if (error) App.outputErrors(error);
+    });
+  },
+  'click .logo': function () {
+    var $navBarItems = $('.navbar .nav li');
+    $navBarItems.removeClass('active');
+    App.changePage('main');
+  },
+  'click .institution': function (event) {
+    var target = $(event.target).parent();
+    if (target.hasClass('add')) {
+      var content = $('.container-create-institution');
+      content.show();
+      $.fancybox({
+        content: content
       });
+    }
+    else {
+      App.changePage('main',true);
+    }
+  }
+});
 
-      _this.$alerts.on('click', 'li', function () {
-        $(this).stop(true, false).animate({
-          opacity: 0,
-          height: 0
-        }, 500, function () {
-          $(this).remove();
-        });
-      });
-    },
+Template.login.events({
+  'click .switch-create': function () {
+    Session.set('loginState', "create");
+  },
+  'click .switch-login': function () {
+    Session.set('loginState', "login");
+  },
+  'click .switch-retrieve': function () {
+    Session.set('loginState', "retrieve");
+  },
+  'click .btn': function () {
+    var state = Session.get('loginState'),
+      $containerLogin = $('.container-login'),
+      email = $containerLogin.find('.login-email').val(),
+      name = $containerLogin.find('.login-name').val(),
+      password = $containerLogin.find('.login-password').val();
 
-    renderPatientlistTemplate: function () {
-      var _this = this;
-
-      var patients_container = $(".patientlist"),
-        list_rendered = false,
-        total_patients = 0,
-        row_split = 8;
-
-      if (patients_container.length) {
-        list_rendered = true;
-        total_patients = patients_container.find('li').length;
-      }
-
-      if (list_rendered && patients_container.hasClass('sortable')) {
-        patients_container.sortable({
-          update: function (event, ui) {
-            _this.updatePatients();
-          }
-        });
-      }
-
-      if (list_rendered && total_patients > row_split) {
-        patients_container.addClass('two_columns');
-      }
-      else if (list_rendered) {
-        patients_container.removeClass('two_columns');
-      }
-    },
-
-    applySortable: function () {
-      var _this = this;
-
-      $(".patientlist").sortable({
-        update: function (event, ui) {
-          _this.updatePatients();
+    if (state == "create") {
+      App.createAccount(name, email, password);
+    }
+    else if (state == "login") {
+      Meteor.loginWithPassword({email: email}, password, function (error) {
+        if (error) App.outputErrors(error);
+        else {
+          $.fancybox.close();
+          App.outputSuccess('You are now logged in.');
+          App.changePage('main');
         }
       });
-    },
+    }
+    else if (state == "retrieve") {
 
-    createAccount: function (name, email, password) {
-      var _this = this;
+    }
+  }
+});
 
-      if (!password || password.length < 5)
-        _this.outputErrors("Your password needs to be at least 5 characters.");
-      else {
-        var $loginContainer = $('.container-login'),
-          $loginBtn = $loginContainer.find('.btn');
-        $loginBtn.hide();
-        _this.spinner = _this.addSpinner($loginContainer.find('.loading')[0]);
-        Accounts.createUser({email: email, password: password, profile: {name: name}}, function (error) {
-          $loginBtn.show();
-          _this.spinner.stop();
-          if (error) _this.outputErrors(error);
-          else {
-            Meteor.loginWithPassword({email: email}, password, function (error) {
-              if (error) _this.outputErrors(error);
-            });
-            $.fancybox.close();
-            _this.outputSuccess('Your account has been created.');
-            _this.changePage('main');
-          }
-        });
-      }
-    },
-
-
-    updatePatients: function () {
-      var _this = this;
-
-      //TODO: move this server side?
-      var patients = $('.patientlist .patient');
-      $.each(patients, function (index, patient) {
-        Meteor.call("update_patient", $(patient).data('id'), index + 1, Session.get('current_institution'), function (error, patient_id) {
-          _this.outputErrors(error);
-        });
-      });
-    },
-
-
-    addSpinner: function (el) {
-      var _this = this;
-
-      //DOM is constantly refreshed by meteor , so we need to create a new spinner everytime, otherwise we get strange behaviors
-      //TODO: use template preserve ?
-      var new_spinner = new Spinner(_this.spinner_opts).spin();
-      $(el).append(new_spinner.el);
-      return new_spinner;
-    },
-
-    changePage: function (newPage, forced) {
-      var _this = this,
-        oldPage = _this.currentPage;
-
-      if(!forced && newPage == _this.currentPage) return;
-
-      if (newPage == "login") {
-        var container = $('.container-' + newPage);
-        container.show();
-        $.fancybox({
-          content: container
-        });
-        return;
-      }
-
-      _this.currentPage = newPage;
-      $('.container-' + oldPage).stop(true, false).slideUp('fast', function () {
-        $('.container-' + newPage).stop(true, false).slideDown('fast');
-      });
-    },
-
-    outputErrors: function (error) {
-      var _this = this,
-        msg = "";
-
+Template.createInstitution.events({
+  'click .btn-add-institution': function () {
+    var name = $('input.institution-name').val(),
+      isPrivate = $('input[name="private"]:checked').val();
+    Meteor.call("create_institution", name, '', isPrivate, function (error) {
       if (error) {
-        if (error.reason) // Account creation error
-          msg = error.reason;
-        else
-          msg = error;
-        _this.$alerts.append('<li class="btn-danger">' + msg + '<span class="close"></span></li>');
-        var item = _this.$alerts.find('li').last();
-
-        _this.animateOutput(item);
+        App.outputErrors(error);
       }
-    },
-
-    outputSuccess: function (msg) {
-      var _this = this;
-
-      _this.$alerts.append('<li class="btn-success">' + msg + '<span class="close"></span></li>');
-      var item = _this.$alerts.find('li').last();
-
-      _this.animateOutput(item);
-    },
-
-    toggleViewMode: function($button) {
-      var _this = this;
-
-      //TODO: Refactor this -> use css transitions with a body class instead
-      if (!_this.viewmode_properties) {
-        _this.viewmode_properties = {
-          'elTitle': $('.navbar-fixed-top'),
-          'elMenu': $('.navbar'),
-          'elContent': $('.content'),
-          'elBody': $('body'),
-          'elLogout': $('.btn.logout'),
-          'elAdminPanel': $('.admin_panel')
-        };
+      else {
+        App.changePage('main');
+        App.outputSuccess('New institution created.');
+        $.fancybox.close();
       }
+    });
+  }
+});
 
-      // Switch back to normal mode
-      if (_this.viewmode_properties.elBody.hasClass('listview')) {
-        _this.viewmode_properties.elTitle.slideDown();
-        _this.viewmode_properties.elMenu.animate({'left': 0});
-        _this.viewmode_properties.elLogout.animate({'left': 0});
-        _this.viewmode_properties.elContent.animate({'paddingLeft': 270});
-        _this.viewmode_properties.elBody.removeClass('listview');
-        if (_this.viewmode_properties.elAdminPanel)
-          _this.viewmode_properties.elAdminPanel.slideDown();
-        $button.html('Switch to list view');
-        $('.description-text.sprite').show();
-        //Switch to list mode
-      } else {
-        _this.viewmode_properties.elTitle.slideUp();
-        _this.viewmode_properties.elMenu.animate({'left': -250});
-        _this.viewmode_properties.elLogout.animate({'left': -250});
-        _this.viewmode_properties.elContent.animate({'paddingLeft': 20});
-        _this.viewmode_properties.elBody.addClass('listview');
-        if (_this.viewmode_properties.elAdminPanel)
-          _this.viewmode_properties.elAdminPanel.slideUp();
-        $button.html('Switch to normal view');
-        $('.description-text.sprite').hide();
-      }
-    },
+Template.institution.events({
+  'click': function () {
+    Session.set("current_institution", this._id);
+    App.updatePatients();
+  }
+});
 
-    animateOutput: function (item) {
-      var _this = this,
-        height = item.height() + 10;
+Template.patient.events({
+  'click .delete': function () {
+    Meteor.call('delete_patient', this._id, Session.get("current_institution"));
+    App.updatePatients();
+  },
+  'click .alert-icon': function () {
+    $.fancybox({
+      content: $('.container-add-alert')
+    });
+  }
+});
 
-      item.css({'height': 0});
-      item.animate({
-        opacity: 1,
-        height: height
-      }, 500, function () {
-        setTimeout(function () {
-          item.animate({
-            opacity: 0,
-            height: 0
-          }, 500, function () {
-            item.remove();
-          });
-        }, 5000);
-      });
-    },
+Template.patientlist.events({
+  'click a.up': function () {
+    if (Session.get("selected_patient_position") > 1) {
+      var switching_patient = Patients.findOne({position: Session.get("selected_patient_position") - 1});
+      Patients.update(Session.get("selected_patient"), {$inc: {position: -1}});
+      Patients.update(switching_patient._id, {$inc: {position: +1}});
+      Session.set("selected_patient_position", Session.get("selected_patient_position") - 1);
+    }
+  },
+  'click a.down': function () {
+    var switching_patient = Patients.findOne({position: Session.get("selected_patient_position") + 1});
+    if (switching_patient) {
+      Patients.update(Session.get("selected_patient"), {$inc: {position: 1}});
+      Patients.update(switching_patient._id, {$inc: {position: -1}});
+      Session.set("selected_patient_position", Session.get("selected_patient_position") + 1);
+    }
+  },
+  'click a.add': function () {
+    App.addPatient($('.addPatient').val());
+//      var name = $('.addPatient').val(),
+//        position = $('.patient').length + 1,
+//        institution = Session.get("current_institution");
+//      if (name && position && institution) {
+//        Meteor.call("create_patient", name, position, institution, function (error, patient_id) {
+//          App.outputErrors(error);
+//        });
+//      }
+  },
+  'click a.repair': function () {
+    App.updatePatients();
+  },
+  'click a.empty': function () {
+    Meteor.call('empty_institution', Session.get("current_institution"), function (res) {
+      App.outputSuccess('Institution is now empty');
+    });
+  },
+  'click a.delete': function () {
+    Meteor.call('delete_institution', Session.get("current_institution"), function (res) {
+      App.outputSuccess('Institution deleted');
+    });
+  },
+  'click a[data-action="save-description"]': function() {
+    var description = $('#description').val();
+    Meteor.call('update_institution', Session.get("current_institution"), {description: description}, function (res) {
+      if(description) App.outputSuccess('Description saved');
+      else App.outputSuccess('Description deleted');
+      $('.description').children().toggleClass('hidden');
+    });
+  },
+  'click a[data-action="cancel-description"]': function() {
+    var $description = $('.description');
+    $description.children().toggleClass('hidden');
+    $description.find('textarea').val($description.children('.description-text').text());
 
-    empty: null
-  };
-})(jQuery);
+  },
+  'click span.description-text': function() {
+    $('p.description').children().toggleClass('hidden');
+    $('#description').trigger('focus');
+  },
+  'click .viewmode': function (e) {
+    App.toggleViewMode($(e.srcElement));
+  },
+  'keyup .addPatient': function (event) {
+    if(event.keyCode == 13) {
+      var $input = $(event.srcElement);
+      App.addPatient($input.val());
+      $input.val('');
+    }
+  }
+});
+
+Template.patient.events({
+  'click': function () {
+    Session.set("selected_patient", this._id);
+    Session.set("selected_patient_position", this.position);
+  }
+});
+
+Template.patientlist.rendered = function () {
+  App.renderPatientlistTemplate();
+};
+
+Meteor.startup(function () {
+  console.log('startup');
+  App.init();
+});
--- meteor-app/client/js/src/subscription.js
+++ meteor-app/client/js/src/subscription.js
@@ -1,21 +1,21 @@
-//CLIENT ACTIONS
-if (Meteor.isClient) {
+Session.set('loginState', "login");
 
-  Session.set('loginState', "login");
-
-  //SUBSCRIPTIONS
-  Meteor.subscribe('institutions', null, function () {
-    //TODO: better way to point to the first element id
+//SUBSCRIPTIONS
+console.log('subscribe 1');
+Meteor.subscribe('institutions', null, function () {
+  //TODO: better way to point to the first element id
+  if(Institutions) {
     var defaultInstitution = Institutions.find({"public": true}, {sort: {name: 1}, limit: 1}).fetch();
     if(defaultInstitution.length) {
       Session.set("current_institution", defaultInstitution[0]._id);
     }
 
+    console.log('subscribe 2');
     Meteor.autosubscribe(function () {
       Meteor.subscribe('patients', Session.get("current_institution"));
       Meteor.subscribe('userData', null, function () {
         //console.log('Userdata:',Meteor.user().profile);
       });
     });
-  });
-}
+  }
+});
