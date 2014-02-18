4912242d9ba51f4efb5fd379ffff0d4036b69168
Alex Beauchemin <alexbeauchemin01@gmail.com>
2014-02-18 11:19:13 -0500

    Simulate db joins for alerts

--- meteor-app/server/publications.js
+++ meteor-app/server/publications.js
@@ -10,6 +10,23 @@ Meteor.publish("userData", function () {
     {fields: {'profile': 1}});
 });
 Meteor.publish("alerts", function () {
-  return Alerts.find({user: this.userId});
+  var alerts =  Alerts.find({user: this.userId});
+
+  //TODO: I don't think upserted data are reactive, verify that
+  alerts.map(function(alert,index) {
+    if(alert && alert.patientId) {
+      var patient = Patients.findOne(alert.patientId);
+      var institution = Institutions.findOne(alert.institutionId);
+      if(patient) {
+        Alerts.upsert({patientId: patient._id}, {$set: { patientName: patient.name}});
+        Alerts.upsert({patientId: patient._id}, {$set: { patientPosition: patient.position}});
+      }
+      if(institution) {
+        Alerts.upsert({institutionId: institution._id}, {$set: { institutionName: institution.name}});
+      }
+    }
+  });
+
+  return alerts;
 });
 
